3307d3dce1b74b88f4986e382839576c5e732acc
maxim <thibodeaumaxim53@gmail.com>
2026-02-15 20:55:47 +0000

    Change .gitignore

--- build/CMakeFiles/Export/d35500daa2069f0f785d9665241ffaef/CookbookWrapperLibTargets-debug.cmake
+++ build/CMakeFiles/Export/d35500daa2069f0f785d9665241ffaef/CookbookWrapperLibTargets-debug.cmake
@@ -9,11 +9,11 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 set_property(TARGET CookbookWrapperLib APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(CookbookWrapperLib PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
-  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/CookbookWrapperLib.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libCookbookWrapperLib.a"
   )
 
 list(APPEND _cmake_import_check_targets CookbookWrapperLib )
-list(APPEND _cmake_import_check_files_for_CookbookWrapperLib "${_IMPORT_PREFIX}/lib/CookbookWrapperLib.lib" )
+list(APPEND _cmake_import_check_files_for_CookbookWrapperLib "${_IMPORT_PREFIX}/lib/libCookbookWrapperLib.a" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: F:/Codes/Cookbook/Cookbook
+# Install script for directory: /home/vboxuser/Desktop/Codes/CookbookWrapperLib/Cookbook
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/CookbookWrapper")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,20 +27,30 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+# Set path to fallback-tool for dependency-resolution.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "F:/Codes/Cookbook/Cookbook/build/CookbookWrapperLib.lib")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/vboxuser/Desktop/Codes/CookbookWrapperLib/Cookbook/build/libCookbookWrapperLib.a")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/CookbookWrapperLib/CookbookWrapperLibTargets.cmake")
     file(DIFFERENT _cmake_export_file_changed FILES
          "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/CookbookWrapperLib/CookbookWrapperLibTargets.cmake"
-         "F:/Codes/Cookbook/Cookbook/build/CMakeFiles/Export/d35500daa2069f0f785d9665241ffaef/CookbookWrapperLibTargets.cmake")
+         "/home/vboxuser/Desktop/Codes/CookbookWrapperLib/Cookbook/build/CMakeFiles/Export/d35500daa2069f0f785d9665241ffaef/CookbookWrapperLibTargets.cmake")
     if(_cmake_export_file_changed)
       file(GLOB _cmake_old_config_files "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/CookbookWrapperLib/CookbookWrapperLibTargets-*.cmake")
       if(_cmake_old_config_files)
@@ -53,16 +63,16 @@ if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT
     endif()
     unset(_cmake_export_file_changed)
   endif()
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/CookbookWrapperLib" TYPE FILE FILES "F:/Codes/Cookbook/Cookbook/build/CMakeFiles/Export/d35500daa2069f0f785d9665241ffaef/CookbookWrapperLibTargets.cmake")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/CookbookWrapperLib" TYPE FILE FILES "/home/vboxuser/Desktop/Codes/CookbookWrapperLib/Cookbook/build/CMakeFiles/Export/d35500daa2069f0f785d9665241ffaef/CookbookWrapperLibTargets.cmake")
   if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/CookbookWrapperLib" TYPE FILE FILES "F:/Codes/Cookbook/Cookbook/build/CMakeFiles/Export/d35500daa2069f0f785d9665241ffaef/CookbookWrapperLibTargets-debug.cmake")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/CookbookWrapperLib" TYPE FILE FILES "/home/vboxuser/Desktop/Codes/CookbookWrapperLib/Cookbook/build/CMakeFiles/Export/d35500daa2069f0f785d9665241ffaef/CookbookWrapperLibTargets-debug.cmake")
   endif()
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "F:/Codes/Cookbook/Cookbook/build/install_local_manifest.txt"
+  file(WRITE "/home/vboxuser/Desktop/Codes/CookbookWrapperLib/Cookbook/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -78,6 +88,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "F:/Codes/Cookbook/Cookbook/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/vboxuser/Desktop/Codes/CookbookWrapperLib/Cookbook/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
